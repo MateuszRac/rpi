8b90ad38d78e60495f68c495a611d316608ebfa6
unknown <mateusz.rac@gmail.com>
2024-03-30 19:32:50 +0100

    BMP test

--- bmp_test.py
+++ bmp_test.py
@@ -3,14 +3,12 @@ import board
 # import digitalio # For use with SPI
 import adafruit_bmp280
 
-# Create sensor object, communicating over the board's default I2C bus
-i2c = board.I2C()   # uses board.SCL and board.SDA
-bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
+
 
 # OR Create sensor object, communicating over the board's default SPI bus
-# spi = board.SPI()
-# bmp_cs = digitalio.DigitalInOut(board.D10)
-# bmp280 = adafruit_bmp280.Adafruit_BMP280_SPI(spi, bmp_cs)
+spi = board.SPI()
+bmp_cs = digitalio.DigitalInOut(board.D10)
+bmp280 = adafruit_bmp280.Adafruit_BMP280_SPI(spi, bmp_cs)
 
 # change this to match the location's pressure (hPa) at sea level
 bmp280.sea_level_pressure = 1013.25
